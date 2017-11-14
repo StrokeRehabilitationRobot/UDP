351d8736747237085c6d57613f86b82f91ae34a2
ngoldfarb <nagoldfarb@wpi.edu>
2017-11-13 23:20:31 -0500

    added comments

--- UDP.py
+++ UDP.py
@@ -9,7 +9,7 @@ import random
 import time
 
 
-class UDP_client():
+class UDP():
     """docstring fo UDP_client."""
     def __init__(self, port):
         self._port = port
@@ -24,19 +24,18 @@ class UDP_client():
         sent = self._sock.sendto(buf_id+buf, self._server_address)
         sys.stderr
         data, server = self._sock.recvfrom(self._port)
+
         return array.array('f',data )
 
 
-udp = UDP_client(9876)
-
-theta =  3.14
-vel = 0
-accel = 0
-
-packet = 4*[200,vel,accel]
-t0 = time.time()
-udp.send_packet(37,packet)
-t1 = time.time()
-print (t1-t0)*1000
-
-
+# udp = UDP(9876)
+#
+# theta =  3.14
+# vel = 0
+# accel = 0
+#
+# packet = 4*[200,vel,accel]
+# t0 = time.time()
+# udp.send_packet(37,packet)
+# t1 = time.time()
+# print (t1-t0)*1000
